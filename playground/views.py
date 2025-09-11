ffce99b2d5374ca2933c468dbc68522ed7d5d116
“Furqan <furqaanqadri@gmail.com>
2025-09-11 15:42:53 +0100

    prefetch_related and select_related update

--- playground/views.py
+++ playground/views.py
@@ -18,7 +18,19 @@ def say_hello(request):
     # query_set = Product.objects.all()
     # query_set=Product.objects.filter(Q(inventory__gt=10)|Q(unit_price__lt=10))
     #comparing fields
-    products= Product.objects.order_by('title','unit_price')
+    # products= Product.objects.order_by('title','unit_price')
+    # products=Product.objects.values('title','unit_price')
     # product=Product.objects.earliest('title')
     # same_inventory_and_price=Product.objects.filter(inventory=F('unit_price'))
+    
+    
+    #selecting related objects
+    #selected_related(1)
+    #prefetch_related(n)
+    # products=Product.objects.select_related('collection').all()
+    #  products=Product.objects.select_related('collection').all()
+    #  can also combine both since both return a queryset
+    products=Product.objects.select_related('collection').prefetch_related('promotions')
+    
+    
     return render(request, 'hello.html', {'name':"Furqan", 'products': list(products)})
