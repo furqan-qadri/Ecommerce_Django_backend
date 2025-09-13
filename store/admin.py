ace5a384fec31f8bfa4afa349a00e4632efb7ac5
“Furqan <furqaanqadri@gmail.com>
2025-09-13 12:56:08 +0100

    add order admin

--- store/admin.py
+++ store/admin.py
@@ -26,6 +26,15 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable=['membership']
     ordering=['first_name','last_name']
     list_per_page=10
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display=['payment_status', 'placed_at', 'customer_full_name']
+    ordering=['placed_at']
+    list_per_page=10
+    list_select_related=['customer']
+
+    def customer_full_name(self, order):
+        return f"{order.customer.first_name} {order.customer.last_name}"
 
 
 admin.site.register(models.Collection)
